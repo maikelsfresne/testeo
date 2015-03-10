a91b78d8811798572779659daca76e5c1fd56aae
cesinolu <cesinolu@gmail.com>
2015-03-10 12:27:54 +0100

    Documentacion añadida a la variable 'documentacion'

--- testeosED/src/testeosED/Hola.java
+++ testeosED/src/testeosED/Hola.java
@@ -3,5 +3,21 @@ package testeosED;
 public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola 2.0");
+		
+		String documentacion=""
+				+ "En la pestaña GitRepositories le damos a clonar repositorio,"
+				+ "introducimos la url del repositorio, nuestro nombre y usuario,"
+				+ "tambien marcamos la casilla de bajar todo el contenido despues"
+				+ "de crear el repositorio."
+				+ "Una vez hecho esto nos habra aparecido el proyecto en nuestra"
+				+ "vista de Package Explorer, podemos realizar cualquier cambio y"
+				+ "despues resubir haciendo click derecho en el proyecto y"
+				+ "'Team/Remote/Push', se nos abrira una ventana el la que "
+				+ "seleccionaremosle la rama a utilizar y la añadiremos, tambien"
+				+ "le damos a force update para que sobreescriba y en finish."
+				+ "De esta forma ya hemos subido el fichero modificado, pero sin"
+				+ "cambiar el comentario, para hacerlo se hacen los mismos pasos"
+				+ "pero en 'Team/commit' en el que lo primero que hara sera"
+				+ "pedirnos el nuevo comentario para la version.";
 	}
 }
